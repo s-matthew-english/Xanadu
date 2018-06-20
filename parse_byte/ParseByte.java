0a67337f3eb0e5a7204235e3516ad97bd939d7ac
S. Matthew English <s.matthew.english@gmail.com>
2018-06-20 10:43:42 -0400

    experimentation

---
+++ parse_byte/ParseByte.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+public class ParseByte {
+	public static void main(String... args) {
+		String zeroCool = "0x07";
+
+		byte twoCool;
+
+		System.out.println("zeroCool: " + zeroCool);
+
+		System.out.println("subZeroCool: " + zeroCool.substring(2));
+
+		twoCool = Byte.parseByte(zeroCool.substring(2));
+
+		System.out.println(twoCool);
+	}
+}
